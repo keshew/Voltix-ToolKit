f161cecc258b1514d29e8a34cbc15becc488ffcd
keshew <artyom.korotckov2017@yandex.ru>
2026-06-22 16:10:28 +0500

    Initial Commit

---
+++ Voltix ToolKit/Voltix_ToolKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voltix_ToolKitApp.swift
+//  Voltix ToolKit
+//
+//  Created by Артём Коротков on 22.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Voltix_ToolKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
